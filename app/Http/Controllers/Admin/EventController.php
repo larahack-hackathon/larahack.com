10873a7cd1fef0ee8f3b4e5cdb90a740af3dd18b
Michael Burton <me@mikeylicio.us>
2020-02-22 20:26:35 +0000

    Fixes for StyleCI(?)

--- app/Http/Controllers/Admin/EventController.php
+++ app/Http/Controllers/Admin/EventController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Requests\EventRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\EventRequest;
 use App\Models\Event;
 use App\Models\EventType;
 
