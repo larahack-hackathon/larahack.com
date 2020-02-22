9d6a7f94cb890c90579dbaca915c974a18153cf2
Michael Burton <me@mikeylicio.us>
2020-02-22 20:44:28 +0000

    StyleCI

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -13,7 +13,7 @@ class TeamController extends Controller
      */
     public function index()
     {
-        return view( 'teams.index' )->with( 'teams', auth()->user()->teams()->withCount('users')->get() );
+        return view('teams.index')->with('teams', auth()->user()->teams()->withCount('users')->get());
     }
 
     /**
